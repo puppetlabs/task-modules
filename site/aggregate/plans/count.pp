99b1a0df6c02dfbc79341a7fd462e155a1baa156
Alex Dreyer <alex@puppetlabs.com>
2018-03-29 14:43:20 -0700

    (maint) remove modules now bundled with bolt

--- site/aggregate/plans/count.pp
+++
@@ -1,9 +0,0 @@
-plan aggregate::count(
-  String $nodes,
-  String $task,
-  Hash[String, Data] $params = {} ){
-
-  $node_array = $nodes.split(',')
-  $res = run_task($task, $node_array, $params)
-  aggregate::count($res)
-}
